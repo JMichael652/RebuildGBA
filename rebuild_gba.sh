cafcb17a4948ad944c72fb00322a2a9fce683dfc
Joshua Redding <jmichael652@gmail.com>
2019-02-19 18:33:02 -0500

    Adds handling of positional arguments

--- rebuild_gba.sh
+++ rebuild_gba.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
-echo 'stub'
+# Handle argument for user Makefile
+if [ "$1" != "" ]; then
+	user_makefile="$1"
+else
+	echo "No user Makefile passed as argument"
+	exit 1
+fi
+
+# Handle argument for student project path
+if [ "$2" != "" ]; then
+	student_dir="$2"
+else
+	student_dir="$(pwd)"
+fi
+
+# Demonstrate results
+printf "User Makefile: %s, Student Directory: %s\n" "$user_makefile" "$student_dir"
 
